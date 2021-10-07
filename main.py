61c0cda06ee13542aea686803b7b6295c5464bc5
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-08 04:33:31 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+Python is a high-level programming language designed to be easy to read and
+simple to implement. It is open source, which means it is free to use, even
+for commercial applications. Python can run on Mac, Windows, and Unix
+systems and has also been ported to Java and .NET virtual machines.
+
+Your welcome fellow coder :)
